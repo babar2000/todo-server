ecc81c742acfc3d6a4e47141e54ac1796818348a
Zain Babar <zainibaba1990@yahoo.com>
2024-03-29 19:01:17 +0500

    todo-server FastAPI

---
+++ todo-server/app/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+from app import settings
+
+todo_server: FastAPI = FastAPI()
+
+@todo_server.get("/")
+def hello():
+    return {"Hello": "World"}
+
+@todo_server.get("/db")
+def db_var():
+    return  {"DB": settings.DATABASE_URL}
