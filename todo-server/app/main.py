cc312b58e25147fc26ed2b0937543ce4d7d66606
Zain Babar <zainibaba1990@yahoo.com>
2024-03-30 23:16:04 +0500

    todo server update with FastAPI

--- todo-server/app/main.py
+++ todo-server/app/main.py
@@ -1,8 +1,34 @@
 from fastapi import FastAPI
-
+from sqlmodel import SQLModel, Field, create_engine, Session
+from contextlib import asynccontextmanager
 from app import settings
 
-todo_server: FastAPI = FastAPI()
+# step:1 Database Table schema
+
+class Todo(SQLModel, table=True):
+    id:int | None = Field(default=None, primary_key=True)
+    title:str
+
+# Connection to the database
+    
+connection_string : str = str (settings.DATABASE_URL).replace("postgresql", "postgresql+psycopg")
+
+engine = create_engine(connection_string)
+
+def create_db_table():
+    print("create_db_tables")
+    SQLModel.metadata.create_all(engine)
+    print("done")
+
+@asynccontextmanager
+async def lifespan(todo_server: FastAPI):
+    print("Server Startup")
+    create_db_table()
+    yield 
+
+# Table Data save, get, update, delete
+
+todo_server: FastAPI = FastAPI(lifespan=lifespan)
 
 @todo_server.get("/")
 def hello():
@@ -10,4 +36,12 @@ def hello():
 
 @todo_server.get("/db")
 def db_var():
-    return  {"DB": settings.DATABASE_URL}
+    return  {"DB": settings.DATABASE_URL, "Connection": connection_string}
+
+@todo_server.post("/todo")
+def create_todo(todo_data: Todo):
+    with Session(engine) as session:
+        session.add(todo_data)
+        session.commit()
+        session.refresh(todo_data)
+        return todo_data
