fd444877fec47c8abcdc238dfd4b895846709a07
Zain Babar <zainibaba1990@yahoo.com>
2024-04-08 00:28:31 +0500

    Fast API and Neon Database Integration with SQL Model and Test cases

--- todo-server/app/main.py
+++ todo-server/app/main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI
-from sqlmodel import SQLModel, Field, create_engine, Session
+from fastapi import FastAPI, Depends
+from typing import Annotated
+from sqlmodel import SQLModel, Field, create_engine, Session, select
 from contextlib import asynccontextmanager
 from app import settings
 
@@ -8,10 +9,12 @@ from app import settings
 class Todo(SQLModel, table=True):
     id:int | None = Field(default=None, primary_key=True)
     title:str
-
+    description: str
+    
 # Connection to the database
     
-connection_string : str = str (settings.DATABASE_URL).replace("postgresql", "postgresql+psycopg")
+connection_string : str = str (settings.DATABASE_URL).replace(
+     "postgresql", "postgresql+psycopg")
 
 engine = create_engine(connection_string)
 
@@ -30,18 +33,37 @@ async def lifespan(todo_server: FastAPI):
 
 todo_server: FastAPI = FastAPI(lifespan=lifespan)
 
-@todo_server.get("/")
-def hello():
-    return {"Hello": "World"}
+
+# @todo_server.get("/")
+# def hello():
+#     return {"Hello": "World"}
 
 @todo_server.get("/db")
 def db_var():
     return  {"DB": settings.DATABASE_URL, "Connection": connection_string}
 
-@todo_server.post("/todo")
-def create_todo(todo_data: Todo):
+def get_session():
     with Session(engine) as session:
-        session.add(todo_data)
+        yield session
+
+@todo_server.get("/")
+def hello_world():
+	return {"Greet": "Hello World"} 
+
+
+@todo_server.post("/todo")
+def create_todo(try_content: Todo, session:Annotated[Session, Depends(get_session)]):
+        session.add(try_content)
         session.commit()
-        session.refresh(todo_data)
-        return todo_data
+        session.refresh(try_content)
+        #session.close
+        return try_content
+    
+# Get all todos data
+@todo_server.get("/todo")
+def get_all_todos(new_concept: Annotated[Session, Depends(get_session)]):
+    #with Session(engine) as session: # will close session automatically
+        # Todos Select
+        query = select(Todo)
+        all_todos = new_concept.exec(query).all()
+        return all_todos
